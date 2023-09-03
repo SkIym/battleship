b16eb49f27a82e317d5d2d353e49f8075e8d8f31
SkIym <abramjosh13@gmail.com>
2023-09-03 12:05:56 +0800

    mend

--- src/js/dom.js
+++ src/js/dom.js
@@ -20,42 +20,6 @@ boards.forEach((b) => {
   }
 })
 
-const buildShip = (length, dir) => {
-  const ship = document.createElement('div');
-  ship.style.display = 'grid';
-  if (dir) {
-    ship.style.gridTemplateColumns = `repeat(${length}, 40px)`;
-    ship.style.gridTemplateRows = `40px`;
-  } else {
-    ship.style.gridTemplateRows = `repeat(${length}, 40px)`;
-    ship.style.gridTemplateColumns = `40px`;
-  }
-  
-  for(let i = 0; i < length; i += 1) {
-    const part = document.createElement('div');
-    part.style.backgroundColor = 'yellow';
-    part.style.border = '1px solid black';
-    part.style.cursor = 'grab';
-    ship.appendChild(part)
-  }
-  return ship
-
-}
-
-export const initPlayerShips = (SHIP_LENGTHS) => {
-
-  SHIP_LENGTHS.forEach((length) => {
-    const dir = Math.random() > 0.5
-    const ship = buildShip(length, dir);
-    ship.style.position = 'absolute';
-    ship.style.top = `calc(${parseInt(Math.random() * 10, 10)} * 10%)`;
-    ship.style.left = `calc(${parseInt(Math.random() * 10, 10)} * 10%)`;
-    
-    playerBoard.appendChild(ship)
-    })
-
-}
-
 
 export const enemyBoard = document.getElementById('computer-board');
 export const enemyTiles = document.querySelectorAll('#computer-board > .tile');
